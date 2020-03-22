4b2ca942ee24850ec4517888c475646c28645512
naijel <naijel.a@gmail.com>
2020-03-22 20:13:27 +0200

    feat: add cats, dream-team and transform array implementation

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr = [];
+
+  if (Array.isArray(members) != true) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      arr.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+
+  return arr.sort().join('');
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Array.isArray(arr) == false) {
+        throw Error();
+    }
+
+    let rdy = [];
+
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] == '--discard-next' && i < arr.length - 1) {
+            i += 1;
+        }
+        else if (arr[i] == '--discard-prev' && i > 0) {
+            rdy.pop();
+        }
+
+        else if (arr[i] == '--double-next' && i < arr.length - 1) {
+            rdy.push(arr[i + 1]);
+        }
+        else if (arr[i] == '--double-prev' && i > 0) {
+            rdy.push(arr[i - 1]);
+        }
+        else if (arr[i] != '--discard-prev' && arr[i] != '--double-prev' && arr[i] != '--double-next' && arr[i] != '--discard-next') {
+            rdy.push(arr[i]);
+        }
+    return rdy;
 };
